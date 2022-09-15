5164257d22a49c563652d46a42b5405033428a96
0nIine <113630452+0nIine@users.noreply.github.com>
2022-09-15 19:55:37 +0200

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello World!";
+
+let myVariable =3
+myVariable===4
+
+document.querySelector('img').addEventListener('click', function () {
+    alert('Ouch! Stop poking me!');
+  });
