5062643cf4cc55471f20b21e170d7b50b88ac42a
0nIine <113630452+0nIine@users.noreply.github.com>
2022-09-15 20:44:42 +0200

    Delete sanctus-first directory

--- sanctus-first/helloworld.js
+++
@@ -1,9 +0,0 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
-
-let myVariable =3
-myVariable===4
-
-document.querySelector('img').addEventListener('click', function () {
-    alert('Ouch! Stop poking me!');
-  });
